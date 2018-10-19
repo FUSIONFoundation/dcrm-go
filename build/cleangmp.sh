2d6767f1176c2e2c8cce629b091eddb9bfe8882f
haijuncai <changsileixiang@163.com>
2018-10-19 21:24:20 +0800

    add cleangmp.sh cleanpbc.sh

---
+++ build/cleangmp.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#Copyright 2018 The fusion-dcrm 
+#Author: caihaijun@fusion.org
+
+cd $PWD/crypto/dcrm/gmp-6.1.2
+make clean 
+
---
+++ build/cleanpbc.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#Copyright 2018 The fusion-dcrm 
+#Author: caihaijun@fusion.org
+
+cd $PWD/crypto/dcrm/pbc-0.5.14
+make clean
